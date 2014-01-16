90d4ce177309fd4d7fe59412dc148f3a8cfad14d
obullxl <obullxl@163.com>
2014-01-16 16:43:48 +0800

    upgrade velocity layout system Signed-off-by: obullxl <obullxl@163.com>

--- sqlite-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dbPath=/home/obullxl/atom-jeesite/WebRoot/public
-dbFile=$dbPath/jeesite.db
-maillist='obullxl@163.com,obullxl@gmail.com'
-
-echo 'sending dbfile......'
-mutt -s '$dbFile' -a $dbFile $maillist
